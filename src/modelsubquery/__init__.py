a788022c402a3a860a3323e99c408718ea98f7d2
Baptiste Mispelon <bmispelon@gmail.com>
2024-05-21 20:10:46 +0200

    Sorted imports with isort

--- src/modelsubquery/__init__.py
+++ src/modelsubquery/__init__.py
@@ -4,4 +4,4 @@ Tools to build subqueries that produce model instances
 
 __version__ = "0.1"
 
-from .functions import JSONModelField, model_to_json, ModelSubquery
+from .functions import JSONModelField, ModelSubquery, model_to_json
